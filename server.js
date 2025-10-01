bd62b6c89343e61cc8c02360235008a3619579c9
Anson Chung <ansonchung3@gmail.com>
2025-10-01 16:22:29 +0100

    Change to package.json to enable imports API key imports. Tested with basic API call. Success.

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
-const express = require('express');
-const path = require('path');
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import { TFL_KEY } from './public/api_keys/TFL_KEY.js'
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Needed because __dirname isn’t available in ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.static(path.join(__dirname)));
+
+app.get('/tfl-test', async (req, res) => {
+    try {
+        const response = await fetch(`https://api.tfl.gov.uk/Place/Meta/PlaceTypes?api_key=${TFL_KEY}`);
+        console.log(response.status);
+        const body = await response.text();
+        console.log(body);
+        res.send(body); // send something back to client
+    } catch (error) {
+        console.log(error)
+    }
+
+})
 
 // this is the default fall back. Keep this at the end of this file
 app.use((req, res) => {
