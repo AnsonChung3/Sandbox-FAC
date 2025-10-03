37bd6ce6fcc0b6fb289ba25637f6eb21a27e11a7
Anson Chung <ansonchung3@gmail.com>
2025-10-03 20:59:59 +0100

    Add tube line id meta to local storage

--- backend/router/TFL-routes.js
+++ backend/router/TFL-routes.js
@@ -14,6 +14,21 @@ const enterLog = function (req, res, next) {
 };
 TFL_ROUTES.use(enterLog);
 
+TFL_ROUTES.get('/meta-lines', async (req, res) => {
+    try {
+        const response = await fetch(`${url}/Line/Mode/tube,dlr?api_key=${TFL_KEY}`);
+        const body = await response.text();
+        console.log(response.status);
+        // console.log(body); // send something back to client
+        const parsedBody = JSON.parse(body);
+        const arrayExtract = parsedBody.filter((item) => item.modeName === 'tube').map((element) => element.id);
+        console.log(arrayExtract);
+        res.send(arrayExtract);
+    } catch (error) {
+        console.log(error)
+    }
+});
+
 TFL_ROUTES.get('/meta', async (req, res) => {
     try {
         const response = await fetch (`${url}/Line/Meta/Modes?api_key=${TFL_KEY}`);
